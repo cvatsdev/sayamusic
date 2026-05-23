b45489140d80ddccecdc5d349b961c7176907cee
CVATSDEV <cvatss.com@gmail.com>
2026-05-23 16:05:17 +0530

    fix bugs

--- VIPMUSIC/plugins/bot/settings.py
+++ VIPMUSIC/plugins/bot/settings.py
@@ -1,12 +1,3 @@
-#
-# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
-#
-# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-#
 from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.errors import MessageNotModified
