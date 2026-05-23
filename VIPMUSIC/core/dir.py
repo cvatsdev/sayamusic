528261ff500d3db281f93c2d323d69bfac1b6ec1
CVATSDEV <cvatss.com@gmail.com>
2026-05-23 15:52:38 +0530

    fix bugs

--- VIPMUSIC/core/dir.py
+++ VIPMUSIC/core/dir.py
@@ -1,11 +1,3 @@
-#
-# Copyright (C) 2021-2022 by KIRU-OP@Github, < https://github.com/KIRU-OP >.
-#
-# This file is part of < https://github.com/KIRU-OP/VIPMUSIC > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/KIRU-OP/VIPMUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
 import logging
 import os
 import sys
